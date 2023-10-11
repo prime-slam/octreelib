6b0a77f65bc91ac2dfdf3a001d3518db0898cdf7
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-10-11 13:10:49 +0300

    add visualization for grid and octree

---
+++ octreelib/visualization/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module contains functions for visualizing Grid and Octree classes
+"""
+
+import octreelib.visualization.views as views_module
+
+from octreelib.visualization.views import *
+
+__all__ = views_module.__all__
