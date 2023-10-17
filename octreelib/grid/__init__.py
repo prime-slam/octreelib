5b2293e1ad8dce21332d8b7493ed19a459f80144
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-10-17 15:46:21 +0300

    implement grid with points

--- octreelib/grid/__init__.py
+++ octreelib/grid/__init__.py
@@ -1,10 +1,16 @@
 """
-This module contains base classes for Grid and GridConfig
+This module contains base classes for Grid, GridConfig, GridWithPoints
 """
 import octreelib.grid.grid_base as grid_base_module
+import octreelib.grid.grid_with_points as grid_with_points_module
 import octreelib.grid.static_grid as static_grid_module
 
 from octreelib.grid.grid_base import *
+from octreelib.grid.grid_with_points import *
 from octreelib.grid.static_grid import *
 
-__all__ = grid_base_module.__all__ + static_grid_module.__all__
+__all__ = (
+    grid_base_module.__all__
+    + static_grid_module.__all__
+    + grid_with_points_module.__all__
+)
