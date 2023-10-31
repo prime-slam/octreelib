dc487e819035777988c2f3a62a42aca8996b0766
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-10-31 19:52:06 +0300

    create Box class and move box_intersection and is_point_inside_box into the class as its methods

--- octreelib/internal/geometry.py
+++ octreelib/internal/geometry.py
@@ -1,16 +1,27 @@
-from typing import Optional
+from __future__ import annotations
+
+from dataclasses import dataclass
 
 import numpy as np
 
-from octreelib.internal import RawPoint, Box
+from octreelib.internal.point import RawPoint
 
 
-def point_is_inside_box(point: RawPoint, box: Box) -> bool:
-    return bool((box[0] <= point).all()) and bool((point <= box[1]).all())
+@dataclass
+class Box:
+    corner_a: RawPoint
+    corner_b: RawPoint
 
+    def __post_init__(self):
+        assert (self.corner_a <= self.corner_b).all()
 
-def boxes_intersection(box_1: Box, box_2: Box) -> Optional[Box]:
-    min_point = np.maximum(box_1[0], box_2[0])
-    max_point = np.minimum(box_1[1], box_2[1])
-    if np.all(min_point < max_point):
-        return min_point, max_point
+    def intersect(self, other: Box) -> Box:
+        min_point = np.maximum(self.corner_a, other.corner_a)
+        max_point = np.minimum(self.corner_b, other.corner_b)
+        if np.all(min_point < max_point):
+            return Box(min_point, max_point)
+
+    def is_point_inside(self, point: RawPoint):
+        return bool((self.corner_a <= point).all()) and bool(
+            (point <= self.corner_b).all()
+        )
