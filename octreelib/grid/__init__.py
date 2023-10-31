4f92e3ea9415210974a625d83665f92d12b9d1a3
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-10-31 14:38:24 +0300

    rename grid_with_points.py to grid.py

--- octreelib/grid/__init__.py
+++ octreelib/grid/__init__.py
@@ -2,9 +2,9 @@
 This module contains base classes and implementations for Grid, GridConfig, GridWithPoints
 """
 import octreelib.grid.grid_base as grid_base_module
-import octreelib.grid.grid_with_points as grid_with_points_module
+import octreelib.grid.grid as grid_with_points_module
 
 from octreelib.grid.grid_base import *
-from octreelib.grid.grid_with_points import *
+from octreelib.grid.grid import *
 
 __all__ = grid_base_module.__all__ + grid_with_points_module.__all__
