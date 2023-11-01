a4195c66250e9522c1cf8dda45b40baba56a7771
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-11-01 14:24:31 +0300

    fix naming and method ordering

--- octreelib/internal/interfaces.py
+++ octreelib/internal/interfaces.py
@@ -22,10 +22,6 @@ class WithID(ABC):
 
     _id_static_counter = 0
 
-    @property
-    def id(self):
-        return self._id
-
     def __init__(self, _id: Optional[int] = None):
         if _id is not None:
             self._id = _id
@@ -33,17 +29,21 @@ class WithID(ABC):
             self._id = WithID._id_static_counter
             WithID._id_static_counter += 1
 
+    @property
+    def id(self):
+        return self._id
+
 
 class WithPoints(ABC):
     """
     This class represents the fact that the object contains points.
     The points are stored in .points field. By default, the field is immutable.
     """
-    @property
-    def _empty_point_cloud(self):
-        return np.empty((0, 3), dtype=float)
-
     def __init__(self, points: Optional[RawPointCloud] = None):
         self._points: RawPointCloud = (
             points if points is not None else self._empty_point_cloud
         )
+
+    @property
+    def _empty_point_cloud(self):
+        return np.empty((0, 3), dtype=float)
