72b666b7f2bb5394040296d3dae2de3f3e37aafc
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-11-01 14:17:06 +0300

    fix naming in internal/box.py

--- octreelib/internal/box.py
+++ octreelib/internal/box.py
@@ -18,20 +18,20 @@ class Box:
     point_a has all minimal coordinates and
     point_b has all maximal coordinates
     """
-    corner_a: RawPoint
-    corner_b: RawPoint
+    corner_min: RawPoint
+    corner_max: RawPoint
 
     def __post_init__(self):
         # check that the coordinates are valid for a box
-        assert (self.corner_a <= self.corner_b).all()
+        assert (self.corner_min <= self.corner_max).all()
 
     def intersect(self, other: Box) -> Box:
         """
         :param other: other box to intersect
         :return: Box, which represents intersection of two boxes
         """
-        min_point = np.maximum(self.corner_a, other.corner_a)
-        max_point = np.minimum(self.corner_b, other.corner_b)
+        min_point = np.maximum(self.corner_min, other.corner_min)
+        max_point = np.minimum(self.corner_max, other.corner_max)
         if np.all(min_point < max_point):
             return Box(min_point, max_point)
 
@@ -40,6 +40,6 @@ class Box:
         :param point: Point to check.
         :return: True if point is inside the box, False if outside.
         """
-        return bool((self.corner_a <= point).all()) and bool(
-            (point <= self.corner_b).all()
+        return bool((self.corner_min <= point).all()) and bool(
+            (point <= self.corner_max).all()
         )
