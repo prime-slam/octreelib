844ec839d304cd3483da64f571940eb1d71f5e69
Mikhail Kiselyov <kiselev.0353@gmail.com>
2023-11-02 11:25:48 +0300

    fix naming

--- octreelib/grid/__init__.py
+++ octreelib/grid/__init__.py
@@ -2,9 +2,9 @@
 This module contains base classes and implementations for Grid, GridConfig
 """
 import octreelib.grid.grid_base as grid_base_module
-import octreelib.grid.grid as grid_with_points_module
+import octreelib.grid.grid as grid_module
 
 from octreelib.grid.grid_base import *
 from octreelib.grid.grid import *
 
-__all__ = grid_base_module.__all__ + grid_with_points_module.__all__
+__all__ = grid_base_module.__all__ + grid_module.__all__
