4b85b8eb35a5409fde329a0a58b760d2a2c4b0d5
Pavel Mokeev <75482510+pmokeev@users.noreply.github.com>
2023-11-05 17:41:35 +0300

    fix(insertion): fix point insertion (#16)
    * fix(insert): fix insert point
    
    * wip

--- octreelib/internal/voxel.py
+++ octreelib/internal/voxel.py
@@ -48,9 +48,7 @@ class VoxelBase(WithID):
         :param point: Point to check.
         :return: True if point is inside the bounding box of a voxel, False if outside.
         """
-        return bool((self.corner_min <= point).all()) and bool(
-            (point <= self.corner_max).all()
-        )
+        return bool((point >= self.corner_min).all()) and bool((point <= self.corner_max).all())
 
     @property
     def corner_min(self):
--- octreelib/octree/multi_pose_octree.py
+++ octreelib/octree/multi_pose_octree.py
@@ -135,7 +135,7 @@ class MultiPoseOctreeNode(OctreeNode):
         if self._has_children:
             for child in self._children:
                 if child.is_point_geometrically_inside(point.without_pose()):
-                    child.insert_points(point)
+                    child._insert_point(point)
         else:
             self._points = self._points.extend(point)
 
