ca541f3c454a49d3565df32697431c8c2443adef
Mikhail Kiselyov <kiselev.0353@gmail.com>
2024-03-23 16:52:55 +0300

    fix comment

--- octreelib/ransac/initial_points_config.py
+++ octreelib/ransac/initial_points_config.py
@@ -1,8 +1,9 @@
 """
 This file contains the configuration for the number of initial points to be used in the RANSAC algorithm.
 This number is used to generate random indices for the initial points.
-It can be configured to generate differen number of initial points.
-It is used by functions inside both the RANSAC class and the util module.
+It can be configured to generate different number of initial points.
+It is used by functions inside both the RANSAC class and the util module
+and because of that it is placed in a separate file.
 """
 
 N_INITIAL_POINTS = 6
