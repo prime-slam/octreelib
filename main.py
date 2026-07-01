b644f645c6c92382a8fc8c18b8a6b37c5d919264
Mikhail Kiselev <kiselev.0353@gmail.com>
2026-07-02 00:46:21 +0200

    fix dependabot warning

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from octreelib!")
+
+
+if __name__ == "__main__":
+    main()
--- test/octree/test_octree.py
+++ test/octree/test_octree.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from octreelib.octree import OctreeNode, Octree, OctreeConfig
 
-
 __all__ = ["test_octree", "test_octree_node"]
 
 
